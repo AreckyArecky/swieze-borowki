e258b3ac6d1d5e41e999c1bb3f131a5c2ed34344
AreckyArecky <kuzma.arkadiusz@gmail.com>
2020-07-06 23:41:10 +0200

    Messing with animations and functions in JS.

--- js/scrit.js
+++ js/scrit.js
@@ -1,7 +1,8 @@
 // mobile menu dropdown
 
-var menuTrigger = document.querySelector(".trigger i");
-var menuTriggerX = document.querySelector(".triggerX i");
+// var menuTrigger = document.querySelector(".trigger i");
+// var menuTriggerX = document.querySelector(".triggerX i");
+var triggerWrap = document.querySelector(".main .triggerWrap");
 var menuElements = document.querySelectorAll(".main ul li");
 
 function menuShow() {
@@ -11,18 +12,23 @@ function menuShow() {
 
 
     }
-    menuTrigger.classList.add('triggerHide');
-    menuTriggerX.classList.add('triggerShow');
+    // menuTrigger.classList.add('triggerHide');
+    // menuTriggerX.classList.add('triggerShow');
+    // menuTrigger.classList.remove('triggerShow');
+    // menuTriggerX.classList.remove('triggerHide');
+    triggerWrap.classList.toggle('triggerSwap');
 }
 
 function menuHide() {
     for (var i = 0; i < menuElements.length; i++) {
         menuElements[i].classList.toggle('menuShow');
     }
-    menuTrigger.classList.remove('triggerHide');
-    menuTriggerX.classList.remove('triggerShow');
-    menuTrigger.classList.add('triggerShow');
-    menuTriggerX.classList.add('triggerHide');
+    // menuTriggerX.classList.add('triggerHide');
+    // menuTrigger.classList.add('triggerShow');
+    // menuTrigger.classList.remove('triggerHide');
+    // menuTriggerX.classList.remove('triggerShow');
+    triggerWrap.classList.toggle('triggerSwap');
 }
-menuTrigger.addEventListener('click', menuShow);
-menuTriggerX.addEventListener('click', menuHide);
+// menuTrigger.addEventListener('click', menuShow);
+// menuTriggerX.addEventListener('click', menuHide);
+triggerWrap.addEventListener('click', menuShow);
